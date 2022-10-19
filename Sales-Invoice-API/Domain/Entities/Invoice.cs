dfbcd7438a7613c8c3d1f33bcd8f2db81b13649e
kamilagruhlke <kgruhlke@gmail.com>
2022-10-19 21:34:11 +0200

    Added domain model and repository

---
+++ Sales-Invoice-API/Domain/Entities/Invoice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Number { get; set; }
+        public int Quantity { get; set; }
+        public decimal NetValue { get; set; }
+        public int Vat { get; set; }
+        public string Description { get; set; }
+        public string Contractor { get; set; }
+
+        public Invoice()
+        {
+        }
+
+        public Invoice(int id, DateTime date, int number, int quantity, decimal netValue,int vat, string description,
+            string contractor)
+        {
+            Id = id;
+            Date = date;
+            Number = number;
+            Quantity = quantity;
+            NetValue = netValue;
+            Vat = vat;
+            Description = description;
+            Contractor = contractor;
+        }
+    }
+}
---
+++ Sales-Invoice-API/Domain/Interfaces/IInvoiceRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IInvoiceRepository
+    {
+        IEnumerable<Invoice> GetAll();
+        Invoice GetById(int id);
+        Invoice Add(Invoice invoice);
+        Invoice Update(Invoice invoice);
+        Invoice Delete(int id);
+    }
+}
